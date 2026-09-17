41434a39c4dbab1537697311ee7de7c3ad728d03
idantitydotme <88442616+idantitydotme@users.noreply.github.com>
2026-09-17 18:54:47 -0300

    refactor: improves rimelight i18n

--- src/i18n/loader.ts
+++
@@ -1,6 +0,0 @@
-import { createTranslationLoader } from "@rimelight/i18n"
-import en from "#i18n/en.json"
-import ro from "#i18n/ro.json"
-import ptBr from "#i18n/pt-br.json"
-
-export default createTranslationLoader({ en, ro, "pt-br": ptBr })
